7c211acea0768149a5f3b9c35177c740d017a11c
nettoscire <32023999+nettoscire@users.noreply.github.com>
2017-09-16 18:52:31 -0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+//var aluno = { 
+//	nome: "Robson",
+//	telefone: "(48) 98809.1807",
+//	idade: 30
+//}
+
+function mudarCor(){
+	var title = document.querySelector("h1");
+	title.innerHTML = "Para de clicar aí";	
+}
+
+
+
+
